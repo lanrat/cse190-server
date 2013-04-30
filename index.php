d91e135f92e762575c52ffad62abaf439f52f687
Sam Rubin <samnrubin@gmail.com>
2013-04-30 16:54:41 -0700

    no

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  return "dbname=d9f15upnpf30jn host=ec2-54-225-106-211.compute-1.amazonaws.com user=whuakxgtjhychs password=6PV1qeLaRHp7PLGcQcfGaZj4mN port=5432 sslmode=require";
-}
- 
 echo "bla bla bla";
 $dsn = "pgsql:"
     . "dbname=d9f15upnpf30jn host=ec2-54-225-106-211.compute-1.amazonaws.com user=whuakxgtjhychs password=6PV1qeLaRHp7PLGcQcfGaZj4mN port=5432 sslmode=require";
