37dcfe82b158cf855ec2f6cc5abf7c812a2b0ea7
Sam Rubin <samnrubin@gmail.com>
2013-04-30 15:59:59 -0700

    "more testing"

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ function pg_connection_string() {
 }
  
 # Establish db connection
-$db = pg_connect(pg_connection_string());
+/*$db = pg_connect(pg_connection_string());
 if (!$db) {
    echo "Database connection error."
    exit;
-}
+}*/
  
-$result = pg_query($db, "SELECT statement goes here");
+//$result = pg_query($db, "SELECT statement goes here");
 echo "bla bla bla";
 ?>
