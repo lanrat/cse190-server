eb13ea18cd9d635a26df5bf8ec3fa3c8eb95f470
Sam Rubin <samnrubin@gmail.com>
2013-04-30 16:08:21 -0700

    employees?

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
-  // we will fill this out next
+  return "dbname=d9f15upnpf30jn host=ec2-54-225-106-211.compute-1.amazonaws.com user=whuakxgtjhychs password=6PV1qeLaRHp7PLGcQcfGaZj4mN port=5432 sslmode=require";
 }
  
 # Establish db connection
@@ -12,6 +12,6 @@ if (!$db) {
    exit;
 }*/
  
-//$result = pg_query($db, "SELECT statement goes here");
+$result = pg_query($db, "SELECT * FROM employees");
 echo "bla bla bla";
 ?>
