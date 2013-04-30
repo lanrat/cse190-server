9a2778cd65b1d9490a8727d9b34acd9060e4f0b4
Sam Rubin <samnrubin@gmail.com>
2013-04-30 16:47:13 -0700

    will it work?

--- index.php
+++ index.php
@@ -5,18 +5,18 @@ function pg_connection_string() {
   return "dbname=d9f15upnpf30jn host=ec2-54-225-106-211.compute-1.amazonaws.com user=whuakxgtjhychs password=6PV1qeLaRHp7PLGcQcfGaZj4mN port=5432 sslmode=require";
 }
  
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-   echo "Database connection error."
-   exit;
-}
- 
-$result = pg_query($db, "SELECT * FROM employees");
 echo "bla bla bla";
+$dsn = "pgsql:"
+    . "dbname=d9f15upnpf30jn host=ec2-54-225-106-211.compute-1.amazonaws.com user=whuakxgtjhychs password=6PV1qeLaRHp7PLGcQcfGaZj4mN port=5432 sslmode=require";
+$db = new PDO($dsn);
 
-while($row = pg_fetch_row($result)){
-  echo "$row[0] $row[1] $row[2] $row[3] \n";
+$query = "SELECT employee_id, last_name, first_name" .
+         "FROM employees";
+
+$result = $db->query($quer);
+
+while($row = $result->fetch(PDO::FETCH_ASSOC)){
+  echo "$row["employee_id"] $row["last_name"] $row["first_name"]\n";
 }
 
 echo "IS DONE YO";
