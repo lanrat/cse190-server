c695509470746e3bd9e61211691c5b8d30260fd9
Sam Rubin <samnrubin@gmail.com>
2013-05-01 10:21:52 -0700

    maybe

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ echo "bla bla bla";
 
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1)"
-       . " sslmode=require";
+  $string = "user=$user password=$pass host=$host dbname=" . substr($path, 1)";
+  $string . " sslmode=require";
+  return $string;
 }
 
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
