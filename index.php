7c362734fcb161946f4acf97aef403ed4af8df20
Sam Rubin <samnrubin@gmail.com>
2013-05-01 10:30:29 -0700

    fetch

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 $result = pg_query($pg_conn, "SELECT employee_id, last_name, first_name FROM employees");
 
-while($row = $result->fetch(PDO::FETCH_ASSOC)){
+while($row = pg_fetch_row($result)){
   echo "cmon man";
   echo $row["employee_id"] . $row["last_name"] . $row["first_name"];
 }
