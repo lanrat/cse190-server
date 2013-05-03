7e8855b6bc583d560f361f3ea6107ae83f539b0b
Sam Rubin <samnrubin@gmail.com>
2013-05-03 14:26:53 -0700

    added server.php

---
+++ server.php
@@ -0,0 +1,33 @@
+<?php
+
+  class Server{
+
+    public function serve(){
+      $method = $_GET['action'];
+      
+      switch($method){
+        case "getFortunesSubmitted":
+          break;
+        case: "getFortune":
+          break;
+        case: "submitVote":
+          break;
+        case: "submitFortune":
+          break;
+        case: "submitFlag":
+          break;
+        case: "submitView":
+          break;
+        default:
+          echo "Default";
+        break;
+      }
+    }
+
+
+
+  }
+
+  $server = new Server;
+  $server->serve();
+?>
