f6c7d9bd90e969b4554fb209f2c1c06e47758e95
Sam Rubin <samnrubin@gmail.com>
2013-05-10 14:45:35 -0700

    dat server

---
+++ jsontest.php
@@ -0,0 +1,28 @@
+<?php
+
+// Create map with request parameters
+$params = array ('json' => json_encode(array ('1')));
+ 
+// Build Http query using params
+$query = http_build_query ($params);
+ 
+// Create Http context details
+$contextData = array (
+                'method' => 'POST',
+                'header' => "Connection: close\r\n".
+                            "Content-Length: ".strlen($query)."\r\n",
+                'content'=> $query );
+ 
+// Create context resource for our request
+$context = stream_context_create (array ( 'http' => $contextData ));
+ 
+// Read page rendered as result of your POST request
+$result =  file_get_contents (
+                  'http://cse-190-fortune.herokuapp.com/server.php?action=getFortunesSubmitted',  // page url
+                  false,
+                  $context);
+ 
+// Server response is now stored in $result variable so you can process it
+
+echo $result;
+?>
--- server.php
+++ server.php
@@ -2,11 +2,30 @@
 
   class Server{
 
+    function pg_connection_string_from_database_url() {
+      extract(parse_url($_ENV["DATABASE_URL"]));
+      $string = "user=$user password=$pass host=$host dbname=" . substr($path, 1);
+      $string . " sslmode=require";
+      return $string;
+    }
+
     public function serve(){
       $method = $_GET['action'];
-      echo  $method;
+      if($method != NULL){
+        $pg_conn = pg_connect(pg_connection_string_from_database_url());
+      }
       switch($method){
         case "getFortunesSubmitted":
+          $fortune = json_decode($_POST['json']);
+          $insert = array($fortune["user"]);
+          $result = pg_prepare($pg_conn, "getFortunesSubmitted", 'SELECT * FROM fortunes WHERE uploader = $1');
+
+
+          $result = pg_execute($pg_conn, "getFortunesSubmitted", $insert);
+          while($row = pg_fetch_row($result)){
+            echo $row[0];
+          }
+          
           break;
         case "getFortune":
           $fortune = json_decode($_POST['json']);
