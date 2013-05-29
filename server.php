bb982961961f22e418a90a8686e8df73688bb942
Sam Rubin <samnrubin@gmail.com>
2013-05-29 11:27:23 -0700

    plz work

--- server.php
+++ server.php
@@ -26,7 +26,7 @@
       $pg_conn = pg_connect($this->heroku_conn());
       $result = pg_prepare($pg_conn, "createUser",
       "INSERT INTO users 
-      VALUES ($1) 
+      SELECT $1 
       WHERE NOT EXISTS(
         SELECT userid FROM users WHERE userid = $1");
       $result = pg_execute($pg_conn, "createUser", $insert);
