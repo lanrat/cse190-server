0d067a1989ab4ec9bbfa7b7a018a5dac0dc46918
matanvr <mvardi@ucsd.edu>
2013-05-29 10:31:25 -0700

    ba

--- server.php
+++ server.php
@@ -113,7 +113,8 @@
         case "submitVote":
           $fortune = json_decode($_POST['json'], true);
           $insert = array($fortune["fortuneid"],  $fortune["user"], $fortune["vote"]);                                
-          $result = pg_prepare($pg_conn, "submitVote", 'UPDATE viewed SET vote = $3 WHERE fortuneid = $1 AND userid = $2 ');
+          $result = pg_prepare($pg_conn, "submitVote",
+           'UPDATE viewed SET vote = $3 WHERE fortuneid = $1 AND userid = $2 AND vote = 0 ');
           $result = pg_execute($pg_conn, "submitVote", $insert);
    
           if($result == false)
