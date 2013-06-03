f869af27024a11d8b613a12444cdfe8093e05756
Sam Rubin <samnrubin@gmail.com>
2013-06-03 10:14:09 -0700

    code cleanup

--- server.php
+++ server.php
@@ -196,11 +196,6 @@
           while ($row = pg_fetch_row($result)) {
             $oldvote = $row[0];
           }
-          echo $oldvote;
-
-
-
-
 
           $result = pg_prepare($pg_conn, "submitVote",
            'UPDATE viewed SET vote = $3 WHERE fortuneid = $1 AND userid = $2 RETURNING vote');
@@ -221,20 +216,19 @@
                 'UPDATE fortunes SET downvote = (downvote - 1) WHERE fortuneid = $1 RETURNING downvote');
                 $result = pg_execute($pg_conn, "downVoteDown", array($fortune["fortuneid"])); 
             }
-
             
-              if($fortune["vote"] == 1)
-              {
-                $result = pg_prepare($pg_conn, "upVote",
-                'UPDATE fortunes SET upvote =  1 + upvote WHERE fortuneid = $1 RETURNING upvote');
-                $result = pg_execute($pg_conn, "upVote", array($fortune["fortuneid"]));
-              }
-              else 
-              {
-                $result = pg_prepare($pg_conn, "downVote",
-                'UPDATE fortunes SET downvote = 1 + downvote WHERE fortuneid = $1 RETURNING downvote');
-                $result = pg_execute($pg_conn, "downVote", array($fortune["fortuneid"]));             
-              }
+            if($fortune["vote"] == 1)
+            {
+              $result = pg_prepare($pg_conn, "upVote",
+              'UPDATE fortunes SET upvote =  1 + upvote WHERE fortuneid = $1 RETURNING upvote');
+              $result = pg_execute($pg_conn, "upVote", array($fortune["fortuneid"]));
+            }
+            else 
+            {
+              $result = pg_prepare($pg_conn, "downVote",
+              'UPDATE fortunes SET downvote = 1 + downvote WHERE fortuneid = $1 RETURNING downvote');
+              $result = pg_execute($pg_conn, "downVote", array($fortune["fortuneid"]));             
+            }
           }
           $this->processResult(pg_fetch_assoc($result));
           break;
