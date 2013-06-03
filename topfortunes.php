a7856fc355fcb2b48c2439302c5496eb6d990667
matanvr <mvardi@ucsd.edu>
2013-06-03 11:01:44 -0700

    A

--- topfortunes.php
+++ topfortunes.php
@@ -1,5 +1,5 @@
 <?php
-echo "bla bla bla";
+
 
 
 function pg_connection_string_from_database_url() {
@@ -12,11 +12,17 @@ function pg_connection_string_from_database_url() {
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 $result = pg_query($pg_conn, "SELECT * FROM fortunes");
-
-while($row = pg_fetch_row($result)){
-  echo $row[0] . " " . $row[1] . " " . $row[2];
+?>
+<table> 
+<?while($row = pg_fetch_row($result)){
+?>
+<tr>
+<?
+  echo $row[0]  " "  $row[1]  " "  $row[2] "\n";
+  ?>
+</tr>
+  <?
 }
 
-
-
 ?>
+</table>
