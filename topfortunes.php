a65c30ed6d4f0db444e48f0702d3946e59c0d519
matanvr <mvardi@ucsd.edu>
2013-06-03 12:17:08 -0700

    A

--- topfortunes.php
+++ topfortunes.php
@@ -14,9 +14,9 @@
 
  <body>
  	
-  <div class="jumbotron subhead">
-  <div class="container" style="background-color:#483D8B;">
-    <h1 style="text-align:center;">Top Fortunes</h1>
+  <div class="jumbotron subhead" style="background-color:#483D8B;">
+  <div class="container" >
+    <h1 style="text-align:center; color:#fff;">Top Fortunes</h1>
 
   </div>
 	</div>
