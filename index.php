77f3244f5658e1224a8aa1cad10be54ca11b32e1
matanvr <mvardi@ucsd.edu>
2013-06-03 11:05:24 -0700

    AAAA

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "bla bla bla";
+
 
 
 function pg_connection_string_from_database_url() {
@@ -11,12 +11,13 @@ function pg_connection_string_from_database_url() {
 
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
-$result = pg_query($pg_conn, "SELECT employee_id, last_name, first_name FROM employees");
+$result = pg_query($pg_conn, "SELECT * FROM fortunes");
 
 while($row = pg_fetch_row($result)){
-  echo $row[0] . " " . $row[1] . " " . $row[2];
+  echo $row[0] . " " . $row[1] . " " . $row[2] "\n" ;
+
 }
 
-echo "IS DONE YO";
+
 
 ?>
